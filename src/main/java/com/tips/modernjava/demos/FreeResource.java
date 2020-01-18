6a53e6776b59636a183cf8e8c2f455529fb89260
Kayode Adechinan <kayode.adechinan@gmail.com>
2020-01-18 04:36:26 -0500

    initial

---
+++ src/main/java/com/tips/modernjava/demos/FreeResource.java
@@ -0,0 +1,15 @@
+package com.tips.modernjava.demos;
+
+
+
+public class FreeResource {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/tips/modernjava/ModernjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tips.modernjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
